96e4d67173ce42a7ab83d8cd8bae75cf9a3310bf
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-08 11:14:43 +0330

    in playground - fetch exactly json with model.

--- MoviesAPI-Practice/Playground/Practice.playground/Contents.swift
+++ MoviesAPI-Practice/Playground/Practice.playground/Contents.swift
@@ -2,7 +2,25 @@ import UIKit
 
 var greeting = "Hello, playground"
 
+// MARK: - Properties
+
 let url = "https://reactnative.dev/movies.json"
+
+// MARK: - Models
+
+struct MovieModel: Decodable {
+    let title: String
+    let description: String
+    let movies: [Movies]
+}
+
+struct Movies: Decodable {
+    let id: String
+    let title: String
+    let releaseYear: String
+}
+
+// MARK: - Fetch
 
 func fetch() {
     
@@ -10,18 +28,22 @@ func fetch() {
     
     let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
         
-        if let error = error {
-            print("Error in fetching: \(error.localizedDescription)")
-        }
+        if let error = error { print("There is fetching error: \(error.localizedDescription)") }
         
         guard let data = data else { return }
         
-        print(data)
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(MovieModel.self, from: data)
+            print(decodedData)
+        } catch {
+            print("Error decodeing data.")
+        }
         
     }
-    
+   
     dataTask.resume()
 }
 
 fetch()
-
