64dfb5c630907a37a8ceb5a36f18546ab761d9a4
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-09 12:38:26 +0330

    create MovieManager.swift and a func inside this for receive jsondata from internet.

---
+++ MoviesAPI-Practice/Controller/TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesAPI-Practice
+//
+//  Created by Omid Heydarzadeh on 2/7/24.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#function)
+    }
+    
+}
+
---
+++ MoviesAPI-Practice/Model/MovieManager.swift
@@ -0,0 +1,36 @@
+//
+//  MovieManager.swift
+//  MoviesAPI-Practice
+//
+//  Created by Omid Heydarzadeh on 2/9/24.
+//
+
+import UIKit
+
+struct MovieManager {
+    
+    func fetchMovies() {
+        
+        guard let url = URL(string: "https://reactnative.dev/movies.json") else { return }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
+            
+            if let error = error {
+                print("Error fetching movies: \(error.localizedDescription)")
+            }
+            
+            guard let jsonData = data else { return }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let decodedData = try decoder.decode(MovieModel.self, from: jsonData)
+            } catch {
+                print("Error decoding data.")
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+}
